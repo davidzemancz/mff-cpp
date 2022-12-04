5eb6010c742d14be1026c493316a8705b192af6e
David Zeman <david.zeman@outlook.com>
2022-12-04 17:19:30 +0100

    opt

--- DataAggregation/DataAggregation.cpp
+++ DataAggregation/DataAggregation.cpp
@@ -7,14 +7,15 @@
 #include <unordered_map>
 #include <vector>
 #include <vector>
-#include <cstdbool>
 #include <iostream>
 #include <sstream>
+#include <string_view>
+#include <charconv>
 
 using namespace std;
 
-enum class ColDataType { Unknown = 0, Int = 1, String = 2, Double = 3 };
 enum class ColAggFun { None = 0, Sum = 1, Min = 2, Max = 3 };
+enum class ColDataType { Unknown = 0, String = 1, Int = 2, Double = 3 };
 
 class Col
 {
@@ -22,118 +23,173 @@ public:
 	size_t m_pos;
 	string m_name;
 	ColDataType m_dataType;
-	ColAggFun m_aggFun;
 
 	Col() {}
-	Col(const size_t pos, const string& name) : m_pos(pos), m_name(name) {}
 	Col(const size_t pos, const string& name, const ColDataType dataType) : m_pos(pos), m_name(name), m_dataType(dataType) {}
-	Col(const size_t pos, const string& name, const ColDataType dataType, const ColAggFun aggFun) : m_pos(pos), m_name(name), m_dataType(dataType), m_aggFun(aggFun) {}
+	virtual ~Col() {}
 };
 
-const string D = ";";
-vector<Col> cols;
-vector<vector<string>> rows;
-string query;
 
-ColDataType strToDataType(string& str) {
-	if (strcmp(str.c_str(), "string") == 0) return ColDataType::String;
-	else if (strcmp(str.c_str(), "int") == 0) return ColDataType::Int;
-	else if (strcmp(str.c_str(), "double") == 0) return ColDataType::Double;
+class AggCol
+{
+public:
+	string m_name;
+	const Col* m_origCol;
+	ColAggFun m_aggFun;
+
+	AggCol() {}
+	AggCol(const string& name, const Col* origCol, const ColAggFun aggFun) : m_name(name), m_origCol(origCol), m_aggFun(aggFun) {}
+	virtual ~AggCol() {}
+};
+
+class IntCol : public Col
+{
+public:
+	vector<int> vals;
+
+	IntCol() {}
+	IntCol(const size_t pos, const string& name, const ColDataType dataType, const size_t size) : Col(pos, name, dataType){ vals.reserve(size); }
+	~IntCol() {}
+};
+
+class DoubleCol : public Col
+{
+public:
+	vector<double> vals;
+
+	DoubleCol() {}
+	DoubleCol(const size_t pos, const string& name, const ColDataType dataType, const size_t size) : Col(pos, name, dataType) { vals.reserve(size); }
+	~DoubleCol() {}
+};
+
+class StringCol : public Col
+{
+public:
+	vector<string> vals;
+
+	StringCol() {}
+	StringCol(const size_t pos, const string& name, const ColDataType dataType, const size_t size) : Col(pos, name, dataType) { vals.reserve(size); }
+	~StringCol() {}
+};
+
+const char D = ';';
+vector<Col*> cols;
+
+ColDataType strToDataType(string_view str) {
+	if (str == "string") return ColDataType::String;
+	else if (str == "int") return ColDataType::Int;
+	else if (str == "double") return ColDataType::Double;
 	return ColDataType::Unknown;
 }
-
-ColAggFun strToAggFun(string& str) {
-	if (strcmp(str.c_str(), "SUM") == 0) return ColAggFun::Sum;
-	else if (strcmp(str.c_str(), "MIN") == 0) return ColAggFun::Min;
-	else if (strcmp(str.c_str(), "MAX") == 0) return ColAggFun::Max;
+ColAggFun strToAggFun(string_view str) {
+	if (str == "SUM") return ColAggFun::Sum;
+	else if (str == "MIN") return ColAggFun::Min;
+	else if (str == "MAX") return ColAggFun::Max;
 	return ColAggFun::None;
+}
+
+Col* getCol(string& name) {
+	for (Col* col : cols) {
+		if (col->m_name == name) return col;
+	}
+	return nullptr;
+}
+
+vector<string_view> splitString(string& str, char sep) {
+	vector<string_view> r;
+	const char* f = str.c_str();
+	size_t s = 0;
+	for(const char* i = f;;i++)
+	{
+		if (*i == 0  || *i == sep) {
+			r.emplace_back(f, s);
+			if (*i == 0) break;
+			f = ++i;
+			s = 0;
+		}
+		s++;
+	}
+	return r;
 }
 
 void readData() 
 {
-	// Column names
-	string str;
-	string colNames;
-	cin >> colNames;
-	stringstream ssNames(colNames);
-	size_t colIndex = 0;
-	while (getline(ssNames, str, ';')) {
-		if (str == "") break;
-		Col col = Col(colIndex++, str);
-		cols.push_back(col);
-	}
 
-	// Column data types
-	string colTypes;
-	cin >> colTypes;
-	stringstream ssTypes(colTypes);
-	colIndex = 0;
-	while (getline(ssTypes, str, ';')) {
-		if (str == "") break;
-		cols[colIndex].m_dataType = strToDataType(str);
-	}
-
-	// Number of rows
+	// Cols
+	string colNamesStr, colTypesStr;
+	cin >> colNamesStr;
+	vector<string_view> colNames = splitString(colNamesStr, D);
+	cin >> colTypesStr;
+	vector<string_view> colTypes = splitString(colTypesStr, D);
 	size_t rowsCount;
 	cin >> rowsCount;
+	size_t colIndex = 0;
+	for (size_t i = 0; i < colNames.size(); i++) {
+		if (colTypes[i] == "int") cols.push_back(new IntCol(colIndex++, string(colNames[i]), strToDataType(colTypes[i]), rowsCount));
+		else if (colTypes[i] == "double") cols.push_back(new DoubleCol(colIndex++, string(colNames[i]), strToDataType(colTypes[i]), rowsCount));
+		else if (colTypes[i] == "string") cols.push_back(new StringCol(colIndex++, string(colNames[i]), strToDataType(colTypes[i]), rowsCount));
+	}
 
-	// rows
-	rows.reserve(rowsCount);
+	// Rows
 	string row;
 	for (size_t r = 0; r < rowsCount; r++) {
 		getline(cin, row);
 		if (row == "") { r--; continue; }
-		rows.push_back(vector<string>());
-		rows[r].reserve(cols.size());
-		stringstream ssRow(row);
-		while (getline(ssRow, str, ';')) {
-			rows[r].push_back(str);
+
+		vector<string_view> cells = splitString(row, D);
+
+		colIndex = 0;
+		for (size_t i = 0; i < cells.size(); i++) {
+			if (cols[colIndex]->m_dataType == ColDataType::String) {
+				StringCol* sCol = dynamic_cast<StringCol*>(cols[colIndex]);
+				sCol->vals.emplace_back(cells[i]);
+			}
+			else if (cols[colIndex]->m_dataType == ColDataType::Int) {
+				IntCol* sCol = dynamic_cast<IntCol*>(cols[colIndex]);
+				int integer;
+				from_chars(cells[i].data(), cells[i].data() + cells[i].size(), integer);
+				sCol->vals.push_back(integer);
+			}
+			else if (cols[colIndex]->m_dataType == ColDataType::Double) {
+				DoubleCol* sCol = dynamic_cast<DoubleCol*>(cols[colIndex]);
+				double doub;
+				from_chars(cells[i].data(), cells[i].data() + cells[i].size(), doub);
+				sCol->vals.push_back(doub);
+			}
+			colIndex++;
 		}
 	}
 
-	// query
-	getline(cin, query);
+	
 }
 
 void printAgg() {
 	string groupBy;
-	vector<string> selects;
-	unordered_map<string, Col> aggCols;
-	unordered_map<string, Col> colsMap;
-
-	for (Col& col : cols) {
-		colsMap[col.m_name] = col;
-	}
+	vector<AggCol> aggCols;
 
 	// SELECT prijmeni, SUM(jmeno), MIN(key), MAX(key), MIN(vek), MAX(vek), SUM(deti), SUM(rodice), MIN(prumer), MAX(prumer) GROUP_BY prijmeni
 
-	// Consts
-	const string SELECT = "SELECT";
-	const string GROUP_BY = "GROUP_BY";
-
 	// Process query
-	stringstream ss(query);
 	string word;
-	size_t count = 0;
-	while (ss >> word) {
+	while (cin >> word) {
 		if (word == "") continue;
-		if (word == SELECT) continue;
+		if (word == "SELECT") continue;
 
 		if (word.back() == ',') {
 			word.pop_back();
 		}
 
-		if (word == GROUP_BY) {
-			ss >> groupBy;
+		if (word == "GROUP_BY") {
+			cin >> groupBy;
 		}
 		else {
-			string aggFunStr = word.substr(0, 3);
-			ColAggFun aggFun = strToAggFun(aggFunStr);
+			ColAggFun aggFun = strToAggFun(string_view(word.c_str(), 3));
 			string colName = aggFun == ColAggFun::None ? word : word.substr(4, word.size() - 5);
-			aggCols[word] = Col(count++, colName, colsMap.at(colName).m_dataType, aggFun);
-			selects.push_back(word);
+			aggCols.emplace_back(word, getCol(colName), aggFun);
 		}
 	}
+
+	size_t b = 0;
 
 	// Groups
 	/*size_t groupByIndex = colsMap.at(groupBy).m_pos;
@@ -238,5 +294,9 @@ int mainLocal()
 
 int main()
 {
-	return mainRecodex();
+	int ret = mainRecodex();
+	for (Col* col : cols) {
+		delete col;
+	}
+	return ret;
 }
